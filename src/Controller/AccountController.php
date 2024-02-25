bf5705335859071dc2f608f4d4a9eb546c923dd6
GustyCrocodile <daavis.r@gmail.com>
2024-02-25 16:46:32 +0200

    account list by client id route

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,24 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Account;
 use App\Entity\Client;
-use App\Repository\ClientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 #[Route('/api/accounts', name: 'accounts_')]
 class AccountController extends AbstractController
 {
     #[Route('/{id}', name: 'list')]
-    public function list(Client $client): JsonResponse
+    public function list(EntityManagerInterface $entityManager, Client $client): JsonResponse
     {
-        // dd($client);
-        // if not found
-        // json error message
-        // i guess
-        return $this->json([
-            'message' => 'Welcome to your account controller!'
-        ]);
+        $accounts = $entityManager->getRepository(Account::class)->findBy(['owner' => $client->getId()]);
+        return $this->json($accounts);
     }
 }
